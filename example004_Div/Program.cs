1af272fe6f99e9c51bf3c8d75c84d32533571d32
Alena <you@example.com>
2022-07-06 19:56:01 +0300

    example004 Div

---
+++ example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿//создать программу деления одного числа на другое
+double numberA = 12;
+// double потому что могут быть числа с остатком
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
