9788696bcfd77b2b472008241c0eb9bc8856c7c2
Alena <you@example.com>
2022-07-06 19:23:30 +0300

    example002 User name new

---
+++ example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите Ваше имя");
+string username = Console.ReadLine();
+//возьми то,что слева и положи в то,что справа
+//= это знак присваивания
+Console.Write("Привет,");
+Console.Write(username);
