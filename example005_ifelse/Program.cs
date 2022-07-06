293ca285bd7baefd0cd9c05d7add815cb826c2cd
Alena <you@example.com>
2022-07-06 22:00:18 +0300

    example005 ifelse

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+// НЕ нужна ; когда условие if else 
+// ToLower- нижний регистр чтобы маша искалась даже если ввод будет большими буквами
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
