145a99ce2f8ec831878a58e3b3b2454edc5c1c25
Alena8800 <97910257+Alena8800@users.noreply.github.com>
2022-07-06 19:10:30 +0300

    Delete example002_HelloUser directory

--- example002_HelloUser/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Введите Ваше имя");
-string username = Console.ReadLine();
-//возьми то,что слева и положи в то,что справа
-//= это знак присваивания
-Console.Write("Привет,");
-Console.Write(username);
